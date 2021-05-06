cc285a1a7afb8ad9f59602f60835e7c499b98b5c
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2021-05-06 14:48:22 +0800

    Create 11417 - String Operation.cpp
    奇怪用了strlen會compiler error = = 我還debug那麼久

---
+++ 11417 - String Operation.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "function.h"
+using namespace std;
+
+Str::Str(char* s){
+    int idx = 1;
+    head = new Char(s[0]);
+    tail = head;
+    Char* temp = head;
+    while(s[idx]!='\0'){
+        temp->next = new Char(s[idx++]);
+        temp = temp->next;
+    }
+    tail = temp;
+}
+
+Str::Str(const Str & a){
+    Char* temp = a.head->next;
+    head = new Char(a.head->text);
+    tail = head;
+    Char* temp1 = head;
+    while(temp!=NULL){
+        temp1->next = new Char(temp->text);
+        temp1 = temp1->next;
+        temp = temp->next;
+    }
+    tail = temp1;
+}
+
+Str& Str::strInsert(const Str & a){
+    Str* cop = new Str(a);
+    tail->next = cop->head;
+    tail = cop->tail;
+    return *this;
+}
